d0c871c6aca929aea3e6d62dc1309d3627f120cd
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2020-10-31 19:10:25 +0700

    Commit heula

---
+++ bin/Operator_Assignment.dart
@@ -0,0 +1,36 @@
+void main(){
+
+    /*
+        Operator Assignent adalah operator yang menggunakan
+        tanda sama dengan ( = ) untuk mengisi sebuah nilai dalam
+        suatu variabel. Operator Assignment digunakan untuk menyatakan
+        suatu perintah atau fungsi perhitungan untuk memberikan nilai
+        pada variabel dalam bentuk yang disingkat
+     */
+
+  int a = 10;
+  // Assignment operator penjumlahan
+  a+=10;
+  print('Nilai dari a = $a');
+
+  int b = 20;
+  // Assignment operator pengurangan
+  b-=10;
+  print('Nilai dari b = $b');
+
+  double c = 20;
+  // Assignment operator perkalian
+  c*=10;
+  print('Nilai dari c = $c');
+
+  double d = 100;
+  // Assignment operator pembagian
+  d/=25;
+  print('Nilai dari d = $d');
+
+  int e = 10;
+  // Assignment operator modulus
+  e %=7;
+  print('Nilai dari e = $e');
+
+}
---
+++ test/Operator_Assignment_test.dart
@@ -0,0 +1,8 @@
+import 'package:Operator_Assignment/Operator_Assignment.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
